3504b12925bb98012c25eb413d6bc54ebcb5f10a
Amish Verma <amishhverma@gmail.com>
2023-04-14 00:53:54 +0530

    Final Changes

--- src/Components/ZeliconEvents.jsx
+++ src/Components/ZeliconEvents.jsx
@@ -5,9 +5,9 @@ export default function ZeliconEvents() {
     <div className={styles.section}>
     <h1 className={styles.heading}> Our events</h1>
     <div className={styles.events}>
-      <img src="assets/zealicon/poster1.png" alt="poster1"/>
-      <img src="assets/zealicon/poster1.png" alt="poster2"/>
-      <img src="assets/zealicon/poster1.png" alt="poster3"/>
+      <img src="assets/zealicon/mighty.png" alt="poster1"/>
+      <img src="assets/zealicon/Robo olympics.png" alt="poster2"/>
+      <img src="assets/zealicon/Tommorow's tesla.png" alt="poster3"/>
     </div>
     </div>
   );
--- src/components/ZealiconFooter.jsx
+++ src/components/ZealiconFooter.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
-
 export default function Footer() {
   return (
-    <footer className="bg-white px-12 pb-10 text-black">
+    <footer className="bg-white px-12 pb-10 text-black sm:ml-[7rem]  sm:mr-[7rem]">
       <div className="mb-24 flex flex-col sm:flex-row items-start sm:gap-28 gap-8">
-        <img src="/logo.svg" alt="" className="w-[328px] h-[84px]"></img>
+        <img src="/logo.svg" alt="" className="w-[230px] h-[60px]"></img>
         <div>
-          <h3 className="mb-2 font-bold text-lg">Address</h3>
-          <p>
+          <h3 className="mb-2 font-bold text-[1rem]">Address</h3>
+          <p className="text-[0.8rem]">
             JSS Academy of Technical Education,
             <br />
             Sector-62, Noida,
@@ -20,13 +19,13 @@ export default function Footer() {
           </p>
         </div>
         <div>
-          <h3 className="mb-2 font-bold text-lg">Get In Touch</h3>
+          <h3 className="mb-2 font-bold text-[1rem] ">Get In Touch</h3>
           <div className="flex gap-[1px]">
-            <Link
+            <Link className="text-[0.8rem]"
               to=""
-              onClick={() => (window.location = "mailto:officialquanta@gmail.com")}
+              onClick={() => (window.location = "mailto:quanta.jssaten@gmail.com")}
             >
-              officialquanta@gmail.com
+              quanta.jssaten@gmail.com
             </Link>
             <img
               src="/arrow_up_right.svg"
@@ -37,26 +36,29 @@ export default function Footer() {
         </div>
         
         <div>
-          <h3 className="mb-2 font-bold text-lg">Socials</h3>
-          <div className="flex flex-col">
-            <Link to="/">Instagram</Link>
-            <Link to="/">LinkedIn</Link>
-            <Link to="/">Discord</Link>
+          <h3 className="mb-2 font-bold text-[1rem]">Socials</h3>
+          <div className="flex flex-col text-[0.8rem]">
+            <a href="https://www.instagram.com/jssquanta/" target="_blank" rel="noreferrer">Instagram</a>
+            <a href="https://www.linkedin.com/company/quanta-jss/mycompany/" target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://discord.com/channels/961519628872450058/961519628872450061" target="_blank" rel="noreferrer">Discord</a>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-between">
+      <div className="flex justify-between text-[0.8rem]">
         <div>© 2023 JSS Quanta</div>
 
-        <Link to="/zealicon" className="flex gap-[1px]">
+        <div className="flex flex-row w-[7rem] cursor-pointer "
+        onClick={() => {
+          window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        }}>
           <img
             src="/arrow_up_left.svg"
             alt=""
             className="w-[20px] h-[20px]"
           ></img>
           <p>Get To Top</p>
-        </Link>
+        </div>
       </div>
     </footer>
   );
